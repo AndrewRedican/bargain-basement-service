7f66f98afcae39661746e19b34a25a7e4beaed35
AndrewRedican <andrew.redican.mejia@gmail.com>
2019-12-01 06:07:18 +0000

    removed unnecesary success prop in response

--- functions/apis/packages/controller.js
+++ functions/apis/packages/controller.js
@@ -2,20 +2,17 @@ const HttpStatus = require('http-status-codes')
 const { get } = require('../../databaseAccess')
 
 exports.getAllPackages = async (req, res) => {
-  let status, data, success, error
+  let status, data, error
   try {
     data = (await get('packages')) || []
-    success = true
     status = HttpStatus.OK
   } catch (err) {
     error = err.message
-    success = false
     status = HttpStatus.INTERNAL_SERVER_ERROR
   }
   return res.status(status).json({
     data: { ...data, count: undefined },
     count: data.count,
-    success,
     error
   })
 }
@@ -24,7 +21,6 @@ exports.addPackages = (req, res) => {
   // todo, nothing found return empty array
   return res.status(200).json({
     // 200 or 204 depending if full details or not
-    success: true,
     data: {
       process: 'addPackages'
     }
@@ -32,32 +28,30 @@ exports.addPackages = (req, res) => {
 }
 
 exports.getPackage = async (req, res) => {
-  let status
-  const body = {}
+  let status, data, error
   try {
     const data = await get(`packages/${req.params.id}`)
     if (data !== null) {
-      body.data = data
-      body.success = true
+      data = data
       status = HttpStatus.OK
     } else {
-      body.error = `Could not find package with id #${req.params.id}`
-      body.success = false
+      error = `Could not find package with id #${req.params.id}`
       status = HttpStatus.NOT_FOUND
     }
   } catch (err) {
-    body.success = false
-    body.error = err.message
+    error = err.message
     status = HttpStatus.INTERNAL_SERVER_ERROR
   }
-  return res.status(status).json(body)
+  return res.status(status).json({
+    data,
+    error
+  })
 }
 
 exports.editPackage = (req, res) => {
   // todo...
   const exists = true
   return res.status(exists ? 200 : 404).json({
-    success: true,
     data: {
       process: 'editPackage',
       targetId: req.params.id
@@ -69,7 +63,6 @@ exports.removePackage = (req, res) => {
   // todo...
   const existed = true
   return res.status(existed ? 204 : 404).json({
-    success: true,
     data: {
       process: 'removePackage',
       targetId: req.params.id
--- functions/apis/products/controller.js
+++ functions/apis/products/controller.js
@@ -5,17 +5,14 @@ exports.getAllProducts = async (req, res) => {
   let status, data, success, error
   try {
     data = (await get('products')) || []
-    success = true
     status = HttpStatus.OK
   } catch (err) {
     error = err.message
-    success = false
     status = HttpStatus.INTERNAL_SERVER_ERROR
   }
   return res.status(status).json({
     data: { ...data, count: undefined },
     count: data.count,
-    success,
     error
   })
 }
@@ -25,10 +22,8 @@ exports.addProducts = async (req, res) => {
   const body = {}
   try {
     insert('products', req.body)
-    body.success = true
     status = HttpStatus.CREATED
   } catch (err) {
-    body.success = false
     body.error = err.message
     status = HttpStatus.INTERNAL_SERVER_ERROR
   }
@@ -42,15 +37,12 @@ exports.getProduct = async (req, res) => {
     const data = await get(`products/${req.params.id}`)
     if (data !== null) {
       body.data = data
-      body.success = true
       status = HttpStatus.OK
     } else {
       body.error = `Could not find product with id #${req.params.id}`
-      body.success = false
       status = HttpStatus.NOT_FOUND
     }
   } catch (err) {
-    body.success = false
     body.error = err.message
     status = HttpStatus.INTERNAL_SERVER_ERROR
   }
@@ -61,7 +53,6 @@ exports.editProduct = (req, res) => {
   // todo...
   const exists = true
   return res.status(exists ? 200 : 404).json({
-    success: true,
     data: {
       process: 'editProduct',
       targetId: req.params.id
